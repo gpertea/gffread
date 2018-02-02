4809223931fa439db63b8307f7424f0b763d9ba9
Geo Pertea <geo.pertea@gmail.com>
2018-02-02 15:52:38 -0500

    updated license to MIT

--- prep_linux.sh
+++ prep_linux.sh
@@ -1,19 +1,22 @@
-#!/bin/sh
+#!/bin/bash -e
 ver=$(fgrep '#define VERSION ' gffread.cpp)
 ver=${ver#*\"}
 ver=${ver%%\"*}
-pack=gffread-$ver
+srcpack=gffread-$ver
+source prep_source.sh
 linpack=$pack.Linux_x86_64
 echo "preparing $linpack.tar.gz"
 echo "-------------------"
 /bin/rm -rf $linpack
 /bin/rm -f $linpack.tar.gz
 mkdir $linpack
+cd $srcpack
 make clean
 make release
-cp gffread $linpack/
+cp LICENSE gffread ../$linpack/
+cd ..
 tar cvfz $linpack.tar.gz $linpack
-ls -l $linpack.tar.gz
-echo "On the salzX servers the web files can be updated like this:"
-echo "cp $linpack.tar.gz $pack.tar.gz  ~/html/software/stringtie/dl/"
-echo "perl -i -pe 's/gffread\-\d\.\d+\.\d+\w?\./gffread-$ver./g' ~/html/software/stringtie/gff.shtml"
+ls -l $srcpack.tar.gz $linpack.tar.gz
+echo "scp $linpack.tar.gz $pack.tar.gz  salz:~/html/software/stringtie/dl/"
+echo ".. then on the server: "
+echo "perl -i -pe 's/gffread\-\d\.\d+\.\d+\w?\./gffread-$ver./g' ~/html/software/stringtie/gff*.shtml"
--- prep_source.sh
+++ prep_source.sh
@@ -3,19 +3,19 @@ ver=$(fgrep '#define VERSION ' gffread.cpp)
 ver=${ver#*\"}
 ver=${ver%%\"*}
 pack=gffread-$ver
-echo "preparing $pack.tar.gz"
-echo "-------------------"
+echo " preparing souce $pack.tar.gz"
+echo "----------------------------------"
 /bin/rm -rf $pack
 /bin/rm -f $pack.tar.gz
 mkdir $pack
 mkdir $pack/gclib
 libdir=$pack/gclib/
 
-cp -p gffread.cpp gff_utils.{h,cpp} $pack/
+cp LICENSE gffread.cpp gff_utils.{h,cpp} $pack/
 sed 's|\.\./gclib|./gclib|' Makefile > $pack/Makefile
 
-cp -p ../gclib/{GVec,GList,GHash}.hh $libdir
-cp -p ../gclib/{GArgs,GBase,gdna,GStr,gff,codons,GFaSeqGet,GFastaIndex}.{h,cpp} $libdir
+cp ../gclib/{GVec,GList,GHash}.hh $libdir
+cp ../gclib/{GArgs,GBase,gdna,GStr,gff,codons,GFaSeqGet,GFastaIndex}.{h,cpp} $libdir
 tar cvfz $pack.tar.gz $pack
 ls -l $pack.tar.gz
 
